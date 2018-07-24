94d323d6f30fc2377efa7bc211991d5bba4891c5
meghamohan <meghamohan1989@gmail.com>
2018-07-24 18:45:14 +0000

    adding testcase file

---
+++ 0x00-restful_api_users/tests/test_models/test_base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python4
+"""
+testcases for base class
+"""
+from datetime import datetime
+import unittest
+import uuid
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """ testcases for basemodel class """
+    def init(self):
+        """ initialize """
+        self.model = BaseModel()
+
+    def testClass(self):
+        """ test the base class instance"""
+        self.assertIsInstance(self.model, BaseModel)
+
+    def testClass2(self):
+        """ test the base class instance"""
+        self.assertIsNotNone(self.model)
+
+    def testId(self):
+        """ test id attribute """
+        self.assertIsInstance(self.model.id, str)
+        self.assertEqual(self.model.id, str(uuid.uuid4(self.model.id)))
+        self.assertIsNotNone(self.model.id)
+
+    def testCreatedAt(self):
+        """ test created_at attribute """
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsNotNone(self.model.created_at)
+
+    def testUpdatedAt(self):
+        """ test updated_at attribute """
+        self.assertIsInstance(self.model.updated_at, datetime)
+        self.assertIsNotNone(self.model.updated_at)
+if __name__ == '__main__':
+    unittest.main()
