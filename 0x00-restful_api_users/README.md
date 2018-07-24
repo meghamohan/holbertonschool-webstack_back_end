974ae8b54b0ed1112ee5b3b9783330131ad93dcd
meghamohan <meghamohan1989@gmail.com>
2018-07-24 05:23:50 +0000

    adding task0

---
+++ 0x00-restful_api_users/README.md
@@ -0,0 +1,9 @@
+In this project you will learn the following
+- How to create a Python package
+- What is Unit testing and how to implement it in a large project
+- How to serialize and deserialize a Class
+- How to write and read a JSON file
+- How to manage datetime
+- What is an UUID
+- What ORM means
+- How to map a Python Class to a MySQL table
---
+++ 0x00-restful_api_users/models/base_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+base model class
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    base model class for other classes
+    """
+    id = None
+    created_at = None
+    updates_at = None
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
