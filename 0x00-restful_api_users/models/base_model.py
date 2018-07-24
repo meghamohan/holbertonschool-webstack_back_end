a62e6aa410a6ae0fe4afaa0cd76353181c78ad32
meghamohan <meghamohan1989@gmail.com>
2018-07-24 05:29:14 +0000

    pep8 fix

--- 0x00-restful_api_users/models/base_model.py
+++ 0x00-restful_api_users/models/base_model.py
@@ -15,6 +15,7 @@ class BaseModel:
     updates_at = None
 
     def __init__(self):
+        """ initializing basemodel """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.utcnow()
