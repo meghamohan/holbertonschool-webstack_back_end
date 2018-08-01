6565bdc519b05200586d10c72944e5b74aebe203
meghamohan <meghamohan1989@gmail.com>
2018-08-01 05:48:57 +0000

    fixed condition for drop_all in init

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -12,14 +12,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 
 
-db_engine = create_engine(
-               'mysql+mysqldb://{}:{}@{}/{}'.format(
-                    os.environ.get('HBNB_YELP_MYSQL_USER'),
-                    os.environ.get('HBNB_YELP_MYSQL_PWD'),
-                    os.environ.get('HBNB_YELP_MYSQL_HOST'),
-                    os.environ.get('HBNB_YELP_MYSQL_DB')))
+config = {'drivername': 'mysql+mysqldb',
+            'username': os.getenv('HBNB_YELP_MYSQL_USER'),
+            'password': os.getenv('HBNB_YELP_MYSQL_PWD'),
+            'host': os.getenv('HBNB_YELP_MYSQL_HOST'),
+            'database':  os.getenv('HBNB_YELP_MYSQL_DB')}
+db_engine = create_engine(URL(**config))
 
-if os.getenv("HBNB_YELP_MYSQL_DB") == "hbtn_yelp_test":
+dbengine = create_engine(
+               'mysql+mysqldb://{}:{}@{}/{}'.format(
+                    os.getenv('HBNB_YELP_MYSQL_USER'),
+                    os.getenv('HBNB_YELP_MYSQL_PWD'),
+                    os.getenv('HBNB_YELP_MYSQL_HOST'),
+                    os.getenv('HBNB_YELP_MYSQL_DB')))
+
+"""if config['database'] == "hbtn_yelp_test":"""
+if os.getenv('HBNB_YELP_ENV') == 'test':
     Base.metadata.drop_all(bind=db_engine)
 Base.metadata.create_all(db_engine)
 db_session = scoped_session(sessionmaker(bind=db_engine,
