124065c812d7a6d67b01b575fc6024f3669e5095
meghamohan <meghamohan1989@gmail.com>
2018-09-30 19:03:23 +0000

    fixing one red check

--- 0x02-restful_api_users/0-warmup_flask.py
+++ 0x02-restful_api_users/0-warmup_flask.py
@@ -10,7 +10,7 @@ app.url_map.strict_slashes = False
 @app.route('/', methods=['GET'])
 def index():
     """ root definition """
-    return 'Holberton School\n'
+    return 'Holberton School'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
