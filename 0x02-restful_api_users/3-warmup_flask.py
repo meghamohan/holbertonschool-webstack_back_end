2c5c0a0860e169f34c17f8ff0ba35c55e76d870e
meghamohan <meghamohan1989@gmail.com>
2018-09-30 23:00:48 +0000

    adding task3

---
+++ 0x02-restful_api_users/3-warmup_flask.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+warmup flask application
+"""
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/hbtn')
+def index():
+    """ /hbtn method """
+    app.url_map.strict_slashes = False
+    data = { "C": "is fun", 
+            "Python": "is cool", 
+            "Sysadmin": "is hiring"
+    }
+    return jsonify(data)
+
+if __name__ == "__main__":
+    h = os.getenv('HBNB_API_HOST')
+    p = int(os.getenv('HBNB_API_PORT'))
+    app.run(host=h, port=p)
