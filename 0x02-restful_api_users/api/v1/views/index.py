b554394d249a56195aa17f5b395a16a8a9f0721f
meghamohan <meghamohan1989@gmail.com>
2018-09-30 23:57:05 +0000

    tweaking to fix red checks

--- 0x02-restful_api_users/api/v1/views/index.py
+++ 0x02-restful_api_users/api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def statusOk():
     """ handler returning status """
     return jsonify({'status': 'OK'})
