b1ba9de15e56fa1b8bac850c523e62366ba5f597
meghamohan <meghamohan1989@gmail.com>
2018-10-01 02:29:26 +0000

    addng task 6

--- 0x02-restful_api_users/api/v1/app.py
+++ 0x02-restful_api_users/api/v1/app.py
@@ -9,6 +9,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """ 404 handler """
+    return jsonify({'error': 'Not found'}), 404
+
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
     h = os.getenv('HBNB_API_HOST')
